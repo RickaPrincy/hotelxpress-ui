d39e002b04565fa99906614d168d230ad0f7b5c9
Donovan <hei.donovan.2@gmail.com>
2023-08-06 11:26:18 +0300

    feat: small about language & color of button

--- src/pages/Profile/AdminProfle/AddRoom.jsx
+++ src/pages/Profile/AdminProfle/AddRoom.jsx
@@ -1,20 +1,20 @@
 function AddRoom() {
     return (
         <div className="bg-white shadow-md w-1/2 p-8 mt-2 mx-auto rounded-lg">
-            <span className="flex justify-center p-4 font-bold text-2xl">Add Room</span>
+            <span className="flex justify-center p-4 font-bold text-2xl">Ajouter Chambre</span>
             <div className="flex flex-col gap-6 justify-center items-center">
                 <div className="flex flex-row">
                     <form className="flex flex-col text-sm gap-6 justify-center items-center">
                         <input type="text" placeholder="Nom" className=' outline-none w-2/3 p-4 border-b-2 border-orange-600' />
                         <input type="text" placeholder='categories' className=' outline-none w-2/3 p-4 border-b-2 border-orange-600' />
-                        <input type="number" placeholder='price_night' className=' outline-none w-2/3 p-4 border-b-2 border-orange-600' />
-                        <input type="number" placeholder='price_hours' className=' outline-none w-2/3 p-4 border-b-2 border-orange-600' />
+                        <input type="number" placeholder='prix nuit' className=' outline-none w-2/3 p-4 border-b-2 border-orange-600' />
+                        <input type="number" placeholder='prix heures' className=' outline-none w-2/3 p-4 border-b-2 border-orange-600' />
                     </form>
                     <div className="flex text-sm gap-6 justify-center items-center">
                         <input type="text" placeholder='description' className=' outline-none w-2/ h-40 p-4 border-b-2 border-orange-600' />
                     </div>
                 </div>
-                <button className="bg-green-500 flex justify-center rounded-lg text-white shadow-md p-4 w-1/2 flex">Put</button>
+                <button className="bg-green-500 flex justify-center rounded-lg text-white shadow-md p-4 w-1/3 mt-4 flex">Ajouter</button>
             </div>
         </div>
     )
